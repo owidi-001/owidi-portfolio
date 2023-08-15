31aa0af281c37e8c259a2fea2cdf638bb90e64cf
Owidi <kevinalex846@gmail.com>
2023-08-15 11:32:06 +0300

    Update

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@ function submitAction() {
     alert('Contact info sent successfully.!');
     document.getElementById("contactForm").reset();
 }
+
+// modal display
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
